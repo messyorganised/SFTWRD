1cbc7a1a1e85d9687808d106f6735e01f85534ee
messyorganised <100069865+messyorganised@users.noreply.github.com>
2023-11-09 22:40:24 +1100

    Add files via upload

---
+++ Software_Install.ps1
@@ -0,0 +1,34 @@
+# Base Software Path
+$ScriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$SoftwareFolderPath = "$($ScriptPath)/Software"
+
+
+# Software Download Links
+#Always remember to put comma for each line except the final line
+$softwareInstalled = @(
+    #Enter your File name here
+
+    "Chrome.exe",
+    "AReader.exe",
+
+    
+)
+
+# Function to download software
+#param [string] = declare the call type as a string
+function InstallSoftware {
+    param (
+        [string]$FileName
+    )
+    start-process "$SoftwareFolderPath\$FileName"
+}
+
+# Start software downloads
+foreach ($i in $softwareInstalled) {
+    InstallSoftware -Filename $i -wait
+    write-host "
+
+    $i install completed.
+
+    "
+}
