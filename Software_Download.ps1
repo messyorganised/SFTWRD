4b901229ec0f3805342260c7dc85eb3aa2aebc6b
messyorganised <100069865+messyorganised@users.noreply.github.com>
2023-11-09 23:05:01 +1100

    Update Software_Download.ps1

--- Software_Download.ps1
+++ Software_Download.ps1
@@ -32,6 +32,6 @@ function Create.New.Folder {
 foreach ($i in $softwareDownloads.GetEnumerator()) {
     Download.Software -downloadLink $i.Value -outputFilename $i.Key
     write-host "
-    Software download completed.
+    $i download completed.
     "
 }
